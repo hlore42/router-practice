eabea67daa73404c6c0f691a4752b166f8ba8486
Hannelore Brun <hannelore.brun@gmail.cim>
2021-02-25 21:10:19 -0600

    pushing latest changes: added boostrap and using that for styling

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
-
 import './App.css';
-import IssueList from './Components/issueList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import IssueList from './Components/issueList';
 import IssueDetail from './Components/IssueDetail';
 import Users from './Components/Users';
-
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <nav>
-          <Link to="/"> Home </Link>
-          <Link to="/issues"> Issues </Link>
-          <Link to="users"> Users </Link>
-        </nav>
-        <Switch>
-          <Route path="/" exact>
-            <h1>Home</h1>
+
+        <Navbar variant="dark" bg="dark" sticky="top" className="mb-5">
+          <Navbar.Brand>Issue Tracker</Navbar.Brand>
+        <Nav>
+          <Nav.Link as={Link} to="/"> Home </Nav.Link>
+          <Nav.Link as={Link} to="/issues"> Issues </Nav.Link>
+          <Nav.Link as={Link} to="users"> Users </Nav.Link>
+        </Nav>
+        </Navbar>
+
+        <Container>
+          <Switch>
+            <Route path="/" exact>
+              <h1>Home</h1>
             </Route>
             <Route path="/issues" exact>
-        <IssueList />
-        </Route>
-        <Route path="/issues/:id">
-          <IssueDetail />
-        </Route>
-        <Route path="/users">
-          <Users />
-        </Route>
-        </Switch>
+              <IssueList />
+            </Route>
+            <Route path="/issues/:id">
+              <IssueDetail />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+          </Switch>
+        </Container>
       </div>
     </Router>
   );
--- src/Components/issue.js
+++ src/Components/issue.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 function Issue(props) {
     const { issue } = props;
     return (
-    <div>
-        <h3><Link to={`/issues/${issue.number}`}>{ issue.title }</Link></h3>
-        <span>#{issue.number} opened at { issue.created_at } by <a href={issue.user.html_url}>{issue.user.login}</a></span>
-        
-    </div>
+        <Card>
+            <Card.Body>
+                <Card.Title><h3><Link to={`/issues/${issue.number}`}>{issue.title}</Link></h3></Card.Title>
+                <Card.Text>
+                    <span>#{issue.number} opened at {issue.created_at} by <a href={issue.user.html_url}>{issue.user.login}</a></span>
+                </Card.Text>
+            </Card.Body >
+        </Card>
+
     )
 }
 export default Issue
